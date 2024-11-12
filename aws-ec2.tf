1c18acb4e32bb54c5333c0fa5a31c1a0dac52762
Munaser Abdi <munasar@Munasers-MacBook-Pro.local>
2024-11-11 20:08:53 -0700

    add terraform output script

--- aws-ec2.tf
+++ aws-ec2.tf
@@ -11,3 +11,14 @@ resource "aws_instance" "web" {
     Name = "dev-ec2"
   }
 }
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
